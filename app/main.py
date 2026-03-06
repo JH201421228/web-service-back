9656104fa2d5a90616c8d96732a16033e5e2d607
JH201421228 <741u741@gmail.com>
2026-03-06 15:35:58 +0900

    fix: cors change

--- app/main.py
+++ app/main.py
@@ -55,6 +55,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://newsbase-da15f.web.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
